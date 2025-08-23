c135e8a5fd5fbc1a395a2fbdb70e49389d2a550c
deepinnothing <deepinnothing@gmail.com>
2025-08-24 01:43:14 +0200

    Added sample projects

--- sample-projects/unreal-engine/google-admob/GoogleAdMobCPP/Source/GoogleAdMobCPP/GoogleAdMobCPP.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GoogleAdMobCPP.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GoogleAdMobCPP, "GoogleAdMobCPP" );
